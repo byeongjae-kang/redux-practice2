1932c7542bf0b21aa3e89922e32d5d166d6d32b7
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-12 09:45:53 -0700

    rendor notification dynamically depending on status of http request

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "./store/UI-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showNotification = useSelector((state) => state.ui.notification);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch(
-      `https://redux-practice2-default-rtdb.europe-west1.firebasedatabase.app/cart.json`,
-      {
-        method: "put",
-        body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!"
+        })
+      );
+      const response = await fetch(
+        "https://redux-practice2-default-rtdb.europe-west1.firebasedatabase.app/",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]);
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!"
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!"
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
+  console.log(showNotification);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {showNotification && (
+        <Notification
+          title={showNotification.title}
+          message={showNotification.message}
+          status={showNotification.status}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
